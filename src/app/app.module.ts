0e0f0e8be3dbea200d17e84a7f3596344412ef46
Nikola <nikoladalchevski@gmail.com>
2019-06-22 19:59:48 +0200

    Clear app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-//import { HeaderComponent } from './header/header.component';
-// import { TypeOfBikesComponent } from './type-of-bikes/type-of-bikes.component';
 import { HttpModule } from '@angular/http';
-
-// import { BikeFeaturesComponent } from './bike-features/bike-features.component';
-// import { SizeCalculatorComponent } from './size-calculator/size-calculator.component';
-// import { ExploreBikesComponent } from './explore-bikes/explore-bikes.component';
-// import { RegisterUserComponent } from './register-user/register-user.component'
 import { NotFoundComponent } from './not-found/not-found.component';
-// import { BikeInfoComponent } from './explore-bikes/bike-info/bike-info.component';
-// import { TypeOfBikeRoadComponent } from './type-of-bikes/type-of-bike-road/type-of-bike-road.component';
-// import { TypeOfBikeMountainComponent } from './type-of-bikes/type-of-bike-mountain/type-of-bike-mountain.component';
-// import { TypeOfBikeHibridComponent } from './type-of-bikes/type-of-bike-hibrid/type-of-bike-hibrid.component';
-// import { BikeFeaturesWheelsizeGearsComponent } from './bike-features/bike-features-wheelsize-gears/bike-features-wheelsize-gears.component';
-// import { BikeFeaturesBrakeSuspenHandelbarComponent } from './bike-features/bike-features-brake-suspen-handelbar/bike-features-brake-suspen-handelbar.component';
-// import { FilterBikesComponent } from './explore-bikes/filter-bikes/filter-bikes.component';
+
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { FormsModule } from '@angular/forms';
-// import { ShowBikesComponent } from './explore-bikes/show-bikes/show-bikes.component'
+// import { FormsModule } from '@angular/forms';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AuthFirebaseService } from './shared services/auth-firebase.service';
-//import { LogInComponent } from './header/log-in/log-in.component';
-//import { UserMenuComponent } from './header/user-menu/user-menu.component';
+
 import { ApRoutingModule } from './app-routing-module/app-routing.module';
-//import { PagerService } from './pager.service';
 
 import { TypeOfBikeModuleModule } from './type-of-bike-module/type-of-bike-module.module';
 import { BikeFeaturesModuleModule } from './bike-features-module/bike-features-module.module';
@@ -49,44 +34,14 @@ import { HttpUserBikesService } from './shared services/http-user-bikes.service'
 @NgModule({
   declarations: [
     AppComponent,
-   // HeaderComponent,
-    // TypeOfBikesComponent,
-    
-    // BikeFeaturesComponent,
-    // SizeCalculatorComponent,
-    // ExploreBikesComponent,
-    // RegisterUserComponent,
     NotFoundComponent,
-    // BikeInfoComponent,
-    // TypeOfBikeRoadComponent,
-    // TypeOfBikeMountainComponent,
-    // TypeOfBikeHibridComponent,
-    // BikeFeaturesWheelsizeGearsComponent,
-    // BikeFeaturesBrakeSuspenHandelbarComponent,
-    // FilterBikesComponent,
-    // ShowBikesComponent,
-   // LogInComponent,
-    //UserMenuComponent
-    
+   
   ],
   imports: [
     BrowserModule,
-    // RouterModule.forRoot([
-    //  // {path: '', component: HomeComponentComponent},
-    //   {path: '', component: TypeOfBikesComponent},
-    //   {path: 'bikeFeatures', component: BikeFeaturesComponent},
-    //   {path: 'sizeCalculator', component: SizeCalculatorComponent},
-    //   {path: 'exploreBikes/:bikename', component: BikeInfoComponent},
-    //   {path: 'exploreBikes', component: ExploreBikesComponent},
-    //   {path: 'registerUser', component: RegisterUserComponent},
-      
-    //   {path: '**', component: NotFoundComponent }
-
-      
-    // ]),
     HttpModule,
     AngularFontAwesomeModule,
-    FormsModule,
+    // FormsModule,
     AngularFireModule.initializeApp(environment.firabase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
@@ -104,7 +59,7 @@ import { HttpUserBikesService } from './shared services/http-user-bikes.service'
     AuthFirebaseService,
     HttpUserBikesService,
    
-    //PagerService
+
   ],
  
   bootstrap: [AppComponent]
