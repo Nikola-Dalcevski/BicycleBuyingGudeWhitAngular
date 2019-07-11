6f63fe258d6decd6de33cdec71fd6aa0c6eb7232
Nikola <nikoladalchevski@gmail.com>
2019-07-11 14:07:58 +0200

    Auto-generated commit

--- out-tsc/src/app/bike.spec.js
+++
@@ -1,7 +0,0 @@
-import { Bike } from './bike';
-describe('Bike', function () {
-    it('should create an instance', function () {
-        expect(new Bike()).toBeTruthy();
-    });
-});
-//# sourceMappingURL=bike.spec.js.map
