b6fac9be25f453dc2fdb68ea3c7baf8225e73c6c
Nikola <nikoladalchevski@gmail.com>
2019-07-11 13:30:34 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  
+  firebase: {
+    apiKey: "AIzaSyDHS_EjNWEykvMI8ZHruhQbZ161P3GonT4",
+    authDomain: "frontend-project-with-firebase.firebaseapp.com",
+    databaseURL: "https://frontend-project-with-firebase.firebaseio.com",
+    projectId: "frontend-project-with-firebase",
+    storageBucket: "frontend-project-with-firebase.appspot.com",
+    messagingSenderId: "697608523280",
+    appId: "1:697608523280:web:7a695e8837c36233"
+  }
 };
